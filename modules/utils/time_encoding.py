b04bb60250e6654e5f59700a571a27ac70f93688
Vladimir Lunic <vladimir.lunic@uni-ulm.de>
2025-09-17 14:32:02 +0200

    Streamline sin embedding

--- modules/utils/time_encoding.py
+++ modules/utils/time_encoding.py
@@ -5,22 +5,36 @@ from torch import Tensor, nn
 
 
 class SinusoidalTimeEmbedding(nn.Module):
-    def __init__(self, emb_dim: int) -> None:
+    def __init__(self, emb_dim: int, max_period: float = 10_000.0) -> None:
+        """
+        Args:
+            emb_dim (int): dims into which to embed time
+            max_period (float, optional): up to which period to embed. Defaults to 10_000.0.
+        """
         super().__init__()
 
         assert emb_dim % 2 == 0, "Embedding dimension needs to be divisible by 2"
 
         self.emb_dim = emb_dim
-        self.log_10k = math.log(10_000)
+
+        # precompute freq consts
+        half_dim = emb_dim // 2
+        exp = -math.log(max_period) * torch.arange(0, half_dim) / half_dim
+        self.register_buffer("freqs", torch.exp(exp))
 
     def forward(self, t: Tensor) -> Tensor:
-        # t = (B,)
-        half_dims = self.emb_dim // 2
+        """
+        Args:
+            t (Tensor): tensor in size (B,)
 
-        t_emb = self.log_10k / (half_dims - 1)
-        t_emb = torch.exp(torch.arange(half_dims, device=t.device) * -t_emb)
-        t_emb = t[:, None] * t_emb[None, :]  # (B, d/2)
+        Returns:
+            Tensor: time embedded to (B, D)
+        """
 
-        t_emb = torch.cat([torch.sin(t_emb), torch.cos(t_emb)], dim=1)  # (B, d)
+        t = t.unsqueeze(1)
+
+        # sin and cos emb
+        angles = t * self.freqs  # type: ignore
+        t_emb = torch.cat([torch.sin(angles), torch.cos(angles)], dim=-1)
 
         return t_emb
