6370a46fb96d753cefec60e41ced974c633f4f5d
Vladimir Lunic <vladimir.lunic@uni-ulm.de>
2025-09-22 10:35:59 +0200

    Fix EMA bug

--- modules/utils/ema.py
+++ modules/utils/ema.py
@@ -7,7 +7,7 @@ from torch import Tensor, nn
 class EMA(nn.Module):
     """Implements the exponential moving average for parameters of a model,
     Applied as:
-        EMA_t = rate * param + (1 - rate) * EMA_{t-1}
+        EMA_t = rate * EMA_{t-1} + (1 - rate) * model_param
     To be used as a model wrapper that updates the ema parameters
     """
 
@@ -15,7 +15,8 @@ class EMA(nn.Module):
         """
         Args:
             model (nn.Module): model for which to do EMA on
-            rate (float, optional): decay rate of EMA. Defaults to 0.999.
+            rate (float, optional): decay rate of EMA. rate -> 1 means slower updates.
+                Defaults to 0.999.
         """
         super().__init__()
 
@@ -51,9 +52,11 @@ class EMA(nn.Module):
         with torch.no_grad():
             params = [p for p in self.model.parameters() if p.requires_grad]
             for ema_p, p in zip(self.ema_t, params):
-                ema_p = rate * p + (1 - rate) * ema_p
+                # shorthand for ema_p = rate * ema_p + (1 - rate) * p
+                # also inplace ema_p update
+                ema_p.sub_((1 - rate) * (ema_p - p))
 
-    def ema_to_model(self) -> None:
+    def to_model(self) -> None:
         """Copy EMA_t into the model. Use once training is done"""
 
         params = [p for p in self.model.parameters() if p.requires_grad]
