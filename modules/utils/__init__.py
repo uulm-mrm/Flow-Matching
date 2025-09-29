3324ffe8b55779557bf8254003dcc2dabd8e04f2
Vladimir Lunic <vladimir.lunic@uni-ulm.de>
2025-09-29 09:39:49 +0200

    Add time dependent modules

--- modules/utils/__init__.py
+++ modules/utils/__init__.py
@@ -1,3 +1,4 @@
 from modules.utils.ema import EMA
+from modules.utils.extensions import TimeDependentModule, TimeDependentSequential
 from modules.utils.norms import FiLM
 from modules.utils.time_encoding import SinusoidalTimeEmbedding
